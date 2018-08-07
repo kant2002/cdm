c678f9cb5ae5aa04973e9d842fe334f9c0296f31
Andrii Kurdiumov <kant2002@gmail.com>
2018-08-08 02:18:59 +0300

    Add ICD10 vocabulary wrapper

---
+++ Cdm.EntityFrameworkCore/ICD10Vocabulary.cs
@@ -0,0 +1,10 @@
+﻿namespace Cdm.EntityFrameworkCore
+{
+    public class ICD10Vocabulary : VocabularyRepositoryBase
+    {
+        public ICD10Vocabulary(CdmDbContext context)
+            : base(context, "34")
+        {
+        }
+    }
+}
---
+++ Cdm.EntityFrameworkCore/VocabularyRepositoryBase.cs
@@ -0,0 +1,35 @@
+﻿using Cdm.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Cdm.EntityFrameworkCore
+{
+    public abstract class VocabularyRepositoryBase
+    {
+        private readonly string vocabularyId;
+
+        protected VocabularyRepositoryBase(CdmDbContext context, string vocabularyId)
+        {
+            if (string.IsNullOrWhiteSpace(vocabularyId))
+            {
+                throw new ArgumentException("message", nameof(vocabularyId));
+            }
+
+            this.Context = context ?? throw new ArgumentNullException(nameof(context));
+            this.vocabularyId = vocabularyId;
+        }
+
+        protected CdmDbContext Context { get; }
+
+        protected async Task<Concept> FindConceptById(int id)
+            => await this.Context.Concepts
+                .FirstOrDefaultAsync(_ => _.Id == id && _.Vocabulary == this.vocabularyId)
+                .ConfigureAwait(false);
+
+        protected async Task<Concept> FindConceptByCode(string code)
+            => await this.Context.Concepts
+                .FirstOrDefaultAsync(_ => _.Code == code && _.Vocabulary == this.vocabularyId)
+                .ConfigureAwait(false);
+    }
+}
